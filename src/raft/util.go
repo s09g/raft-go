db4dd34f79827478af2bd6e7868009180d75edcc
zhangshiqiu <zhangshiqiu1993@gmail.com>
2021-08-15 22:32:00 -0700

    remove atomic operations

--- src/raft/util.go
+++ src/raft/util.go
@@ -6,52 +6,13 @@ import (
 )
 
 // Debugging
-const Debug = false
+const Debug = true
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
 		log.Printf(format, a...)
 	}
 	return
-}
-
-func (rf *Raft) getCurrentTerm() int {
-	rf.mu.Lock()
-	term := rf.currentTerm
-	rf.mu.Unlock()
-	return term
-}
-
-func (rf *Raft) setCurrentTerm(term int) {
-	rf.mu.Lock()
-	rf.currentTerm = term
-	rf.mu.Unlock()
-}
-
-func (rf *Raft) incCurrentTerm()  {
-	rf.mu.Lock()
-	rf.currentTerm++
-	rf.mu.Unlock()
-}
-
-func (rf *Raft) getVotedFor() int {
-	rf.mu.Lock()
-	votedFor := rf.votedFor
-	rf.mu.Unlock()
-	return votedFor
-}
-
-func (rf *Raft) setVotedFor(votedFor int) {
-	rf.mu.Lock()
-	rf.votedFor = votedFor
-	rf.mu.Unlock()
-}
-
-
-func (rf *Raft) updateLastHeartBeat() {
-	rf.mu.Lock()
-	rf.lastHeatBeat = time.Now()
-	rf.mu.Unlock()
 }
 
 func (rf *Raft) timeSinceLastHeartBeat() time.Duration {
@@ -61,16 +22,9 @@ func (rf *Raft) timeSinceLastHeartBeat() time.Duration {
 	return time.Since(lastHeatBeat)
 }
 
-
 func (rf *Raft) getRaftState() RaftState {
 	rf.mu.Lock()
 	state := rf.state
 	rf.mu.Unlock()
 	return state
-}
-
-func (rf *Raft) setRaftState(state RaftState) {
-	rf.mu.Lock()
-	rf.state = state
-	rf.mu.Unlock()
 }
